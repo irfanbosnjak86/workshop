6f92557f15b09f9692d661bb4c904f9b8c90dfe7
mobutuseseku <irfan.bosnjak86@gmail.com>
2017-02-20 00:07:42 +0100

    repairs: Adds Form and basic styles for creating the repairs.

--- app/controllers/repairs_controller.rb
+++ app/controllers/repairs_controller.rb
@@ -1,4 +1,20 @@
 class RepairsController < ApplicationController 
   def index
   end
+
+  def new
+    @repair = Repair.new
+  end
+
+  def create
+    @repair = Repair.new(repair_params)
+    @repair.save
+    redirect_to repairs_path
+  end
+
+  private 
+
+  def repair_params
+    params.require(:repair).permit(:title, :malfunction_desc, :repair_desc, :repair_date, :status, :price)
+  end
 end
---
+++ spec/factories/repairs.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :repair do
+    title "MyString"
+    description "MyText"
+    price "9.99"
+  end
+end
