8a23561cf22c40c8601812fa29fa4dfc1d13a46d
mobutuseseku <irfan.bosnjak86@gmail.com>
2017-11-13 02:51:54 +0100

    add-styles-to-app: Adds navbar with sample actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Registration temporary disabled
-  devise_for :users, :skip => :registration
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "workshops#index"
